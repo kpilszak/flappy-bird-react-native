a39c565231cf9278d9922efced3c3cacb2b7ed83
kpilszak <klaudia.pilszak@gmail.com>
2022-10-26 21:25:00 +0200

    Add bird component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Bird } from './components/Bird'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
+      <Bird />
     </View>
   );
 }
---
+++ components/Bird.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+
+const Bird = () => {
+    return (
+        <View style={{
+            position: 'absolute',
+            backgroundColor: 'blue',
+            width: 50,
+            height: 60
+        }} />
+    )
+}
+
+export default Bird;
